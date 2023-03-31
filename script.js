feb6a72bb6147f3967e6331ec244af90307067ae
Nicole Monje <>
2023-03-31 22:44:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function saludo() {
+  alert("Hola mundo");
+  var nombre = prompt("Ingrese su nombre: ");
+  alert("hola " + nombre);
+}
+
+var inicio = document.getElementsByClassName("link")[0];
+
+inicio.addEventListener("click", function() {
+  saludo();
+});
+
+var acerca =
+document.getElementsByClassName("link")[1]; 
+
+acerca.addEventListener("click", function() {
+  alert("gracias por visitar la pagina");
+}); 
